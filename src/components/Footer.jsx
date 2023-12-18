e668a46930e7db130a76dca426b0574918d09504
jissele <gisaldias.m@gmail.com>
2023-12-18 14:34:31 -0300

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "../styles/footer.css";
+
+const Footer = () => {
+    return (
+        <div>
+            <footer class="footer">
+                <div class="footer__container">
+                    <p class="footer__copyright">
+                        © 2023 Películas y Documentales &mdash; All Rights Reserved.
+                    </p>
+                </div>
+            </footer>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/views/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import BGImgae from '../components/home5.jpg'
+import "../styles/home.css"
+export function capitalizarPrimeraLetra(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+const Home = () => {
+
+  return (
+    <div className='home' style={{ backgroundImage: `url('${BGImgae}')` }}>
+      <div className='tittle'>
+        <h1>
+          "Toda la información que pueda ser <br />recopilada en beneficio de las <br /> generaciones futuras debe recogerse <br />ahora o la oportunidad se perderá <br /> para siempre"
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
